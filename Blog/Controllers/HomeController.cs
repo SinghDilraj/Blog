07713c2dcbf64713640afb3857efac48278ead4b
Dilraj Singh <dilrajkehra@gmail.com>
2019-03-07 14:35:38 -0600

    deleted all unneccessary files and folders. Added Layout page i.e. shared header and footers for all pages.

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -8,25 +8,18 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        [Authorize]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
