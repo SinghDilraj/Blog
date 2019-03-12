7b8334d6cf64d354ae368482ad53c3d114551f6c
Dilraj Singh <dilrajkehra@gmail.com>
2019-03-12 12:58:55 -0500

    reupload all files

---
+++ Blog/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Blog
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/js/jquery.validate.min",
+                      "~/vendor/jquery/jquery.min.js",
+                      "~/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                      "~/js/clean-blog.min.js",
+                      "~/js/jqBootstrapValidation.js",
+                      "~/js/contact_me.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                      "~/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/vendor/fontawesome-free/css/all.min.css",
+                      "~/css/clean-blog.min.css"));
+        }
+    }
+}
---
+++ Blog/Models/ViewModels/CreateHomeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models.ViewModels
+{
+    public class CreateHomeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public bool Published { get; set; }
+
+        //[Required]
+        public HttpPostedFileBase Image { get; set; }
+
+        public DateTimeOffset DateCreated { get; set;}
+
+        public DateTimeOffset DateUpdated {get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
