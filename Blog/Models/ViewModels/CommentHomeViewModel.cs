4bdc299cb8894557a4a32ee9bba4d761763a06bd
Dilraj Singh <dilrajkehra@gmail.com>
2019-03-18 09:07:24 -0500

    minor changes to comment functionality

---
+++ Blog/Models/ViewModels/CommentHomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models.ViewModels
+{
+    public class CommentHomeViewModel
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTimeOffset DateUpdated { get; set; }
+        public string ModifyingReason { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+        public virtual BlogPost BlogPost { get; set; }
+        public int BlogPostId { get; set; }
+    }
+}
